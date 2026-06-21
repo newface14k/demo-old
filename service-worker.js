2234cb68bb4af08a07df1707fb563a61d74e50aa
newface14k <44581421+newface14k@users.noreply.github.com>
2026-06-21 22:03:02 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,67 @@
+const CACHE_VERSION = "v2";
+const CACHE_NAME = `timemark-cache-${CACHE_VERSION}`;
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/assets/icon.png"
+];
+
+// install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName)),
+      ),
+    ).then(() => self.clients.claim())
+  );
+});
+
+// fetch (offline support)
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const requestURL = new URL(event.request.url);
+
+  if (requestURL.origin === self.location.origin && event.request.destination === "document") {
+    event.respondWith(
+      fetch(event.request)
+        .then((networkResponse) => {
+          const clonedResponse = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clonedResponse));
+          return networkResponse;
+        })
+        .catch(() => caches.match(event.request)),
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        if (requestURL.origin === self.location.origin) {
+          const clonedResponse = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clonedResponse));
+        }
+        return networkResponse;
+      });
+    }),
+  );
+});
